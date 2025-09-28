ce44f5483873c5259d3634b98c736422998243b6
88chinu <chinmaya.pa42@gmail.com>
2025-09-28 17:55:51 +0000

    add todoform

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -10,13 +10,34 @@ function Todo({ todo }) {
   )
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([
     { text: "Learn React" },
     { text: "Go to friend's house" },
     { text: "Build ToDo List" },
   ])
-
   return (
     <div className="app">
       <div className="todo-list">
