5720f5b8033bd4a7521b4a975f2245a06ca0dca2
88chinu <chinmaya.pa42@gmail.com>
2025-09-28 17:48:10 +0000

    update toDoList

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
+function Todo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      {todo.text}
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn React" },
+    { text: "Go to friend's house" },
+    { text: "Build ToDo List" },
+  ])
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
