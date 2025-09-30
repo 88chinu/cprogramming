d1a3b93edb3d0e5af55408eec86c4d8a0dc59295
88chinu <chinmaya.pa42@gmail.com>
2025-09-30 17:37:06 +0000

    add productcard part

---
+++ e_commerce_site/src/components/ProductCard/productcard.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import {
+  useWishlistContext,
+  useCartContext,
+  useProductContext,
+} from "../../context/AllContextIndex";
+
+import { addToCart, removeFromCart } from "../../services/CartServices";
+import { addToWishlist, deleteWishList } from "../../services/WishlistServices";
+
+import "./ProductCard.css";
+
+function ProductCard({ productCardData }) {
+  const { _id, image, title, price, rating, inStock } = productCardData;
+
+  const { wish } = useWishlistContext();
+  const { dispatch } = useProductContext();
+  const { cart } = useCartContext();
+
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="relative p-4 flex justify-center rounded-xl cursor-pointer">
+        {/* Stock badge */}
+        <div className="top-0 left-0 mt-3 px-2 rounded-lg absolute z-30 bg-green-500 text-gray-100 text-xs md:text-sm font-medium md:block">
+          {inStock === true ? "In Stock" : "Out of Stock"}
+        </div>
+
+        <div className="top-0 left-0 h-2 md:h-3 mt-5 px-2 absolute z-20 bg-green-500"></div>
+        <div className="top-0 left-0 h-2 md:h-3 mt-6 pl-5 rounded-3xl absolute z-0 bg-green-600"></div>
+
+        <div className="w-52 pb-2 bg-white rounded-xl z-10">
+          {/* Product Image */}
+          <div className="relative">
+            <img
+              src={image}
+              className="max-h-60 object-cover rounded-t-xl"
+              alt={title}
+            />
+          </div>
+
+          {/* Product Info */}
+          <div className="px-2 py-1">
+            <Link to={`/Productdetails/${_id}`}>
+              <div className="text-sm md:text-base font-bold pr-2">{title}</div>
+</Link>
+
+            {/* Price & Rating */}
+            <div className="flex py-2">
+              <div className="bg-gray-200 p-1 mr-2 rounded-full text-xs font-medium text-gray-900">
+                ${price}k
+              </div>
+              <div className="flex justify-between item-center">
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-3 md:h-5 md:w-5 text-yellow-500"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <p className="text-gray-600 font-bold text-xs md:text-sm ml-1">
+                    {rating}
+                    <span className="text-gray-500 font-normal">
+                      {" "}
+                      (76 reviews)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Buttons */}
+            <div className="flex flex-col justify-center items-center p-4">
+              {/* Cart Button */}
+              {cart.some((prod) => prod._id === productCardData._id) ? (
+                <button
+                  className="p-1 flex mb-2 justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                  onClick={() => removeFromCart(productCardData._id, dispatch)}
+                >
+                  CART-
+                  <span className="material-icons">remove_shopping_cart</span>
+                </button>
+              ) : (
+                <button
+                  className="p-1 flex mb-2 justify-center items-center bg-gray-700 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                  onClick={() => addToCart(productCardData, dispatch)}
+                >
+                  CART+
+                  <span className="material-icons">shopping_cart</span>
+                </button>
+              )}
+
+              {/* Wishlist Button */}
+              {wish.some((wishlist) => wishlist._id === productCardData._id) ? (
+                <button
+                  className="p-1 flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                  onClick={() =>
+                    deleteWishList(productCardData._id, dispatch)
+                  }
+                >
+                  WISHLIST-
+                  <span className="material-icons">favorite</span>
+                </button>
+              ) : (
+                <button
+                  className="p-1 flex justify-center items-center bg-gray-700 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                  onClick={() => addToWishlist(productCardData, dispatch)}
+                >
+                  WISHLIST+
+                  <span className="material-icons">favorite_border</span>
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
